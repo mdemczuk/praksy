e0773b1e1ecdfa1a5da57c3fbad50f37aa8b12a8
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-08-04 22:58:53 +0200

    added course image
    Course image is displayed above title of the course. Image and title of the course are in one block (<div>) and whole block is a hyperlink to another page.

--- index.php
+++ index.php
@@ -4,30 +4,27 @@
 
 	require_once "connect.php";
 
-	$connection = @new mysqli($host, $db_user, $db_pswd, $db_name);
+	$connection = @new mysqli($host, $db_user, $db_pswd, $db_name);		# connect do db
 
-	if($connection->connect_errno!=0) {	
+	if($connection->connect_errno!=0) {					# if the connection hasn't been established
 		echo "Error: ".$connection->connect_errno;
 	}
-
-	else {
-		$sql = "SELECT * FROM courses";
+	else {									# if the connection has been established
+		$sql = "SELECT id, title, course_img FROM courses";		# sql query to find data of all of the courses
 
 		if($result = @$connection->query($sql)) {
 			$_SESSION['no_courses'] = $result->num_rows;
-		}
 
-		$sql = "SELECT id, title FROM courses";
-
-		if($result = @$connection->query($sql)) {
 			$no_courses = $result->num_rows;
 			$titles = array();
 			$ids = array();
+			$images = array();
 			if($no_courses > 0) {
-				for($x = 0; $x < $no_courses; $x++) {
+				for($x = 0; $x < $no_courses; $x++) {		# fetch data from all rows
 					$course = $result->fetch_assoc();
 					array_push($titles, $course['title']);
 					array_push($ids, $course['id']);
+					array_push($images, $course['course_img']);
 				}
 			} 
 		}
@@ -43,16 +40,43 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>Ideas4learning</title>
+	<meta name="keywords" content="bioinformatics, bioinfo, courses, IT, programming, biology, genomics">
+
+	<style>
+		h1 {
+			text-align: center;
+		}
+		a {
+			text-decoration: none;			/* hyperlink is not underlined */
+		}
+		a:visited {					/* visited hyperlink */
+			color: #404040;
+		}
+		#course {					/* div named 'course' */
+			text-align: center;
+			text-decoration: none;
+		}
+		#course:hover {
+			color: white;
+			background-color: #404040;
+		}
+	</style>
+
 </head>
 
 <body>
-	<h1 align="center">All courses:</h1>
+	<h1>All courses:</h1>
 
 	<?php
 		if($no_courses > 0) {
 				for($x = 0; $x < $no_courses; $x++) {
 					$bin=$titles[$x];
-					echo "<h1 align='center'><a href='course.php?courseid={$ids[$x]}'>$bin</a></h1>";
+					# display a block containing course title and course image; whole block is a hyperlink to another page
+					echo "<a href='course.php?courseid={$ids[$x]}'>
+							<div id='course'>".
+							'<img src="data:image;base64,'.base64_encode($images[$x]).'" alt="Image">'.
+							"<h2>$bin</h2> </div>'
+						</a>";
 				}
 			}
 			else {
