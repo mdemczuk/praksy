e21f9763486983d93dd3d2f2b90c84a5f77ca79a
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-08-10 19:23:44 +0200

    rough draft of administration panel

---
+++ adminpanel.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['loggedin']) || !((isset($_SESSION['admin'])) && ($_SESSION['admin']==true))) {
+		header('Location: index.php');
+		exit();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Ideas4learning</title>
+</head>
+
+<body>
+
+	<h2>Administration panel</h2>
+
+	Add course <br />
+	Delete course <br />
+	Change course info/content <br />
+
+</body>
+
+</html>
