b9a54ffafc408da92cc79a8df00cca7493043924
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-08-10 19:24:17 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,14 @@
 <?php
+
 	session_start();
+
+	if((isset($_SESSION['admin'])) && ($_SESSION['admin']==true)) {
+		$where = "admin.php";
+	}
+	else {
+		$where = "index.php";
+	}
+	
 	session_unset();
-	header('Location:course.php');
+	header("Location: $where");
 ?>
