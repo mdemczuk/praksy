c1edc0649881f15e5dfa21cf5fe1b9e17499e4dd
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-08-11 16:46:57 +0200

    Add files via upload

---
+++ loginpanel.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+
+	$id = $_SESSION['courseid'];
+	if((isset($_SESSION["loggedin$id"])) && ($_SESSION["loggedin$id"]==true)){		# checking if the person is already logged in for this course
+		header("Location: coursecontent.php?courseid=$id");
+		exit();
+	}
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Log In - Ideas4Learning</title>
+</head>
+
+<body>
+	<h3>Login for the course</h3>
+
+	<form action="login.php" method="post">
+		E-mail:<br/><input type="text" name="email" /><br/>
+		Course password:<br/><input type="password" name="pswd" /><br/>
+		<br/><input type="submit" value="Log in" />
+	</form>
+
+	<?php
+		# if variable $_SESSION['error'] exists in this session, the warning will be shown below the login form
+		if(isset($_SESSION['error'])) {
+			echo $_SESSION['error'];
+			unset($_SESSION['error']);
+		}
+	?>
+
+</body>
+</html>
