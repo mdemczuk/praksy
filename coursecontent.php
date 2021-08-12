35ce870d8a404b64c1e40f3ffc36588219f93a0f
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-08-12 17:34:14 +0200

    added a page to change password for the course

--- coursecontent.php
+++ coursecontent.php
@@ -8,6 +8,12 @@
 		exit();
 	}
 
+	if(isset($_SESSION['message'])){
+		$message = $_SESSION['message'];
+		echo "$message";
+		unset($_SESSION['message']);
+	}
+
 	require_once "connect.php";
 
 	$connection = @new mysqli($host, $db_user, $db_pswd, $db_name);
@@ -35,10 +41,16 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>Ideas4Learning</title>
+											<!-- tutaj jest moja próba zrobienia w css tego napisu do zmiany hasła na górze po prawej stronie ale coś mi nie wyszło więc jakbyście wiedzieli jak to zrobić to byłoby super XD -->
+	<style>
+		#change-password {
+			text-align:  center;
+		}
+	</style>
 </head>
 
 <body>
-	<h2>Course content:</h2>
+	<h2>Course content:</h2><div class="change-password"><a href="passchange.php">Change password</a></div>
 
 	<?php
 		echo "<p align = left>".$content."</p>";
@@ -46,5 +58,4 @@
 	?>
 
 </body>
-
 </html>
